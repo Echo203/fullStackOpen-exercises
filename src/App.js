fe42d7c6e1e8ed48d747e9eea770f5c758a53be4
Echo203 <75540650+Echo203@users.noreply.github.com>
2021-07-19 15:06:52 +0200

    Im working here - 2.5 Forms

--- src/App.js
+++ src/App.js
@@ -1,51 +1,15 @@
 import React from 'react'
+import Note from './components/Note'
 
-const Header = (props) => {
-  return (
-  <h1>{props.heading1}</h1>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercise}
-    </p>
-  )
-}
-
-
-const Content = (props) => {
-  return (
-    <>
-      <Part part={props.part1} exercise={props.exercise1} />
-      <Part part={props.part2} exercise={props.exercise2} />
-      <Part part={props.part3} exercise={props.exercise3} />
-    </>
-  )
-}
-
-const Total = (props) => {
-  return(
-    <p>Number of exercises {props.ex1 + props.ex2 + props.ex3}</p>
-  )
-}
-
-
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+const App = ({ notes }) => {
   return (
     <div>
-      <Header heading1 = {course} />
-      <Content part1 = {part1} exercise1 = {exercises1} part2 = {part2} exercise2 = {exercises2} part3 = {part3} exercise3 = {exercises3}/>
-      <Total ex1 = {exercises1} ex2 = {exercises2} ex3 = {exercises3} /> 
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+            <Note key={note.id} note={note} />
+        )}
+      </ul>
     </div>
   )
 }
