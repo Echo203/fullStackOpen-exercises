142e1ed579a99159f9e9778a0344e724120625df
Echo203 <75540650+Echo203@users.noreply.github.com>
2021-07-20 15:21:45 +0200

    Done whole tut on forms

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(false)
+
+  const notesToShow = showAll
+    ?  notes
+    :  notes.filter((note) => note.important === true)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      id: notes.length +1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+      <button onClick={() => setShowAll(!showAll)}>
+        Show {showAll ? 'Important' : 'All'}
+      </button>
+      </div>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
             <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form> 
     </div>
   )
 }
